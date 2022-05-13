bb7488706233796d11df94b360fc102651595735
Jenni Goberski <101295467+jennigoberski@users.noreply.github.com>
2022-05-13 12:01:49 -0300

    Adding more exercises

---
+++ aula-07/js/scripts.js
@@ -0,0 +1,9 @@
+function teste() {
+    var nome = window.prompt("Informe seu nome: ");
+    var sobrenome = window.prompt("Informe seu sobrenome: ");
+    var idade = window.prompt("Informe sua idade: ");
+
+    window.alert(`Olá, ${nome} ${sobrenome}! Você tem ${idade} anos!`);
+    //document.getElementById("info").innerHTML = `Olá, ${nome} ${sobrenome}! Você tem ${idade} anos!`;
+}
+
