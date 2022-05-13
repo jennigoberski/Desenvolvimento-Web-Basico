a903f5c206becf45040a6dedffd53b2e134aa5c9
Jenni Goberski <101295467+jennigoberski@users.noreply.github.com>
2022-05-13 12:00:58 -0300

    Delete js directory

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-function teste() {
-    var nome = window.prompt("Informe seu nome: ");
-    var sobrenome = window.prompt("Informe seu sobrenome: ");
-    var idade = window.prompt("Informe sua idade: ");
-
-    window.alert(`Olá, ${nome} ${sobrenome}! Você tem ${idade} anos!`);
-    //document.getElementById("info").innerHTML = `Olá, ${nome} ${sobrenome}! Você tem ${idade} anos!`;
-}
-
